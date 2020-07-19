b615e7f34d7fbe955556672ff70521b1d38b8959
肖森林 <782766667@qq.com>
2020-07-19 13:26:44 +0800

    first commit

---
+++ dbConn.py
@@ -0,0 +1,11 @@
+import pymysql
+
+db = pymysql.connect("localhost","root","Xsl123456*","HrAPP")
+
+cursor = db.cursor()
+
+cursor.execute("select * from Employer")
+
+data = cursor.fetchall()
+
+print(data)
